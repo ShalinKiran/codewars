133029f85ee1d3350ea66e13940ceefe6d44ae41
Shalin Singaravel <shalinkiran98@gmail.com>
2025-12-14 15:08:33 -0500

    Add solution - CodeHub

---
+++ 6-kyu/stop-gninnips-my-sdrow/stop-gninnips-my-sdrow.js
@@ -0,0 +1,15 @@
+function spinWords(backward) {
+​
+  var sentence = "";
+  var separate = backward.split(" ");
+  
+  for (var i = 0; i < separate.length; i++) {
+    if (sentence) sentence += ' ';
+    if (separate[i].length >= 5) {
+      sentence += separate[i].split("").reverse().join("");
+    } else {
+      sentence += separate[i];
+    }
+  }
+  return sentence;
+}
