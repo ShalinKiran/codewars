a2615d66558b6a6371b9619ea8210cb74c7f67d5
Shalin Singaravel <shalinkiran98@gmail.com>
2026-01-12 00:27:31 -0500

    Add solution - CodeHub

---
+++ 8-kyu/online-rpg-player-to-qualifying-stage/online-rpg-player-to-qualifying-stage.js
@@ -0,0 +1,5 @@
+function playerRankUp (points) {
+  //Use if statements or ternary operator to check if the points scored are higher than 100
+  //Return if its true or false
+  return 100 <= points ? 'Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up.' : false 
+}
